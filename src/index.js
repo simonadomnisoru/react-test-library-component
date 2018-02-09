45c6ac8f0ab359c205b225009cd14e596e90d400
Simona Domnisoru <simona.domnisoru@odigeo.org>
2018-02-09 12:03:16 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class TestLibraryCompoenet extends React.Component {
+  render() {
+    return (
+      <div>Hello there. If you see this, you have successfully publish the library.</div>
+    );
+  }
+}
+export default TestLibraryCompoenet;
